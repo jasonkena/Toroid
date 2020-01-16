c561c7acf6f5d3acfc58a5eef006ccd4fef2a841
jasonkena <jasonice6@gmail.com>
2020-01-16 07:21:08 +0700

    Solved everything

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ import torch.nn.functional as F
 import toroid
 import parser
 
-
-def distance_cube(size):
-    return torch.from_numpy(toroid.distance_cube(size.numpy())).type(torch.FloatTensor)
-
+def softmax(raw_grid):
+    return
 
 def distance_grid(size):
     return torch.from_numpy(toroid.distance_grid(size.numpy())).type(torch.FloatTensor)
@@ -21,45 +19,18 @@ def read(filename):
     return torch.from_numpy(size), torch.from_numpy(grid).type(torch.FloatTensor)
 
 
-def super_cube(size, raw_grid):
-    # raw_grid = F.softmax(raw_grid, dim=0)
-    raw_grid = torch.unsqueeze(raw_grid, dim=0)
-    raw_grid = raw_grid.expand(torch.prod(size), -1, -1)
-    return raw_grid
-
-
 def default(size):
     return torch.eye(torch.prod(size))
 
 
-def loss(size, raw_grid, distance_cube):
-    # breakpoint()
-    value = super_cube(size, raw_grid) * distance_cube
-    # value = super_cube(size, raw_grid)
-    value = torch.sum(value, dim=1)
-    # value = torch.triu(value)
-    # COMMENTED
-    # value = torch.sum(value)
-    return value
-
-
-# PLEASE REFORMAT
-# def hyperloss(size, raw_grid, distance_cube):
-# return torch.sum(
-# loss(size, raw_grid, distance_cube)
-# * loss(size, torch.eye(torch.prod(size)), distance_cube)
-# )
-
-# OPTIMIZE THIS
-def hyperloss(size, raw_grid, distance_cube):
-    value = loss(size, raw_grid, distance_cube)
-    # * loss(size, torch.eye(torch.prod(size)), distance_cube)
-    # * distance_grid
-    value = torch.triu(value)
-    return value
+def loss(raw_grid, distance_grid):
+    result = torch.einsum(
+        "ab,ac,bd,cd->ab", distance_grid, raw_grid, raw_grid, distance_grid
+    )
+    return torch.triu(result)
 
 
 if __name__ == "__main__":
-    size, grid = read("sample2")
-    a = hyperloss(size, grid, distance_cube(size))
+    size, grid = read("sample3")
+    a = loss(grid, distance_grid(size))
 
